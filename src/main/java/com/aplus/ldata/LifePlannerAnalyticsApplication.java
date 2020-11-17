65d1b8242eab1d832c5b6be93563e9d2977683bf
samuelowino <samuelowino43@yahoo.com>
2020-11-17 05:53:18 +0300

    Take off

---
+++ src/main/java/com/aplus/ldata/LifePlannerAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.aplus.ldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePlannerAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifePlannerAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplus/ldata/LifePlannerAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplus.ldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePlannerAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
